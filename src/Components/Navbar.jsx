8a45e1089eba71b8ca1017f1ea15472b9f527065
mrsantokhs <mrsantokhs@gmail.com>
2021-07-09 23:42:21 +1200

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-md">
+          <a className="navbar-brand" href="#">
+            Fashion Box
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
